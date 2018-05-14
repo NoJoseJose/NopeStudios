ef9892e9c6e442578303d170a81b18ba8888e4ee
Dylan Goldman <Dylan.Goldman4@gmail.com>
2018-05-13 22:47:39 -0500

    particles

--- Platyformer/Assets/GameMaster.cs
+++ Platyformer/Assets/GameMaster.cs
@@ -1,12 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Playables;
 
 public class GameMaster : MonoBehaviour
 {
     public static GameMaster gm;
-    public Transform playerPrefab;
-    public Transform spawnPoint;
+    public Transform PlayerPrefab;
+    public Transform SpawnPoint;
+    public float SpawnDelay = 1f;
+    public float killParticlesTime = 3f;
+    public Transform SpawnPrefab;
+    public AudioClip RespawnAudio;
+    public float RespawnAudioVolume = 0.5f;
 
     void Start()
     {
@@ -16,11 +22,15 @@ public class GameMaster : MonoBehaviour
     public void KillPlayer(Player player)
     {
         Destroy(player.gameObject);
-        gm.RespawnPlayer();
+        gm.StartCoroutine(gm.RespawnPlayer());
     }
 
-    public void RespawnPlayer()
+    public IEnumerator RespawnPlayer()
     {
-        Instantiate(playerPrefab, spawnPoint.position, spawnPoint.rotation);
+        AudioSource.PlayClipAtPoint(RespawnAudio, new Vector3(SpawnPoint.position.x, SpawnPoint.position.y, SpawnPoint.position.z), RespawnAudioVolume);
+        yield return new WaitForSeconds(SpawnDelay);
+        Instantiate(PlayerPrefab, SpawnPoint.position, SpawnPoint.rotation);
+        GameObject clone = Instantiate(SpawnPrefab, SpawnPoint.position, SpawnPoint.rotation).gameObject;
+        Destroy(clone, killParticlesTime);
     }
 }
