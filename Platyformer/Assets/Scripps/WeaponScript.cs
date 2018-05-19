89003f504f0968d4e18acf452b2f2c0238187dd9
Dylan Goldman <Dylan.Goldman4@gmail.com>
2018-05-19 16:11:21 -0500

    thru 17

--- Platyformer/Assets/Scripps/WeaponScript.cs
+++ Platyformer/Assets/Scripps/WeaponScript.cs
@@ -1,92 +1,123 @@
-﻿using System;
+﻿using UnityEngine;
 using System.Collections;
-using System.Collections.Generic;
-using JetBrains.Annotations;
-using UnityEngine;
-using Random = System.Random;
 
 public class WeaponScript : MonoBehaviour
 {
     public float fireRate = 0;
     public int Damage = 10;
-    public float fireDistance = 100;
-    public LayerMask ToHit;
+    public LayerMask whatToHit;
+
     public Transform BulletTrailPrefab;
+    public Transform HitPrefab;
     public Transform MuzzleFlashPrefab;
+    float timeToSpawnEffect = 0;
     public float effectSpawnRate = 10;
 
-    private float timeToFire = 0;
-    private float timeToSpawnEffect = 0;
-    private Transform firePoint;
+    // Handle camera shaking
+    public float camShakeAmt = 0.05f;
+    public float camShakeLength = 0.1f;
 
+    float timeToFire = 0;
+    Transform firePoint;
+
+    // Use this for initialization
     void Awake()
     {
         firePoint = transform.Find("FirePoint");
         if (firePoint == null)
         {
-            Debug.LogError("There is no firepoint.");
+            Debug.LogError("No firePoint? WHAT?!");
         }
     }
 
-	// Use this for initialization
-	void Start ()
-	{
-		
-	}
-	
-	// Update is called once per frame
-	void Update ()
-	{
-	    if (fireRate == 0)
-	    {
-            if (Input.GetButtonDown("Fire1"))
-	        {
-	            Shoot();
-	        }
-	    }
-	    else
-	    {
-	        if (Input.GetButton("Fire1") && Time.time > timeToFire)
-	        {
-	            timeToFire = Time.time + (1/fireRate);
-	            Shoot();
-	        }
-	    }
-	}
-
-    private void Shoot()
+    void Start()
     {
-        Vector3 cam = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        Vector2 mousePosition = new Vector2(cam.x, cam.y);
-        Vector2 firePointPosition = new Vector2(firePoint.position.x, firePoint.position.y);
-        RaycastHit2D hit = Physics2D.Raycast( firePointPosition, mousePosition - firePointPosition, fireDistance, ToHit);
 
-        //draw the particle effect
-        if (Time.time >= timeToSpawnEffect)
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (fireRate == 0)
         {
-            DrawBulletTracer();
-            timeToSpawnEffect = Time.time + (1 / effectSpawnRate);
+            if (Input.GetButtonDown("Fire1"))
+            {
+                Shoot();
+            }
         }
-        
+        else
+        {
+            if (Input.GetButton("Fire1") && Time.time > timeToFire)
+            {
+                timeToFire = Time.time + 1 / fireRate;
+                Shoot();
+            }
+        }
+    }
+
+    void Shoot()
+    {
+        Vector2 mousePosition = new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y);
+        Vector2 firePointPosition = new Vector2(firePoint.position.x, firePoint.position.y);
+        RaycastHit2D hit = Physics2D.Raycast(firePointPosition, mousePosition - firePointPosition, 100, whatToHit);
+
+        Debug.DrawLine(firePointPosition, (mousePosition - firePointPosition) * 100, Color.cyan);
         if (hit.collider != null)
         {
+            Debug.DrawLine(firePointPosition, hit.point, Color.red);
             Enemy enemy = hit.collider.GetComponent<Enemy>();
             if (enemy != null)
             {
                 enemy.DamageEnemy(Damage);
-                Debug.Log($"We hit {hit.collider.name} and did {Damage} damage");
+                Debug.Log("We hit " + hit.collider.name + " and did " + Damage + " damage.");
             }
         }
 
+        if (Time.time >= timeToSpawnEffect)
+        {
+            Vector3 hitPos;
+            Vector3 hitNormal;
+
+            if (hit.collider == null)
+            {
+                hitPos = (mousePosition - firePointPosition) * 30;
+                hitNormal = new Vector3(9999, 9999, 9999);
+            }
+            else
+            {
+                hitPos = hit.point;
+                hitNormal = hit.normal;
+            }
+
+            Effect(hitPos, hitNormal);
+            timeToSpawnEffect = Time.time + 1 / effectSpawnRate;
+        }
     }
 
-    private void DrawBulletTracer()
+    void Effect(Vector3 hitPos, Vector3 hitNormal)
     {
-        Instantiate(BulletTrailPrefab, firePoint.position, firePoint.rotation);
-        Transform clone = Instantiate(MuzzleFlashPrefab, firePoint.position, firePoint.rotation);
+        Transform trail = Instantiate(BulletTrailPrefab, firePoint.position, firePoint.rotation) as Transform;
+        LineRenderer lr = trail.GetComponent<LineRenderer>();
+
+        if (lr != null)
+        {
+            lr.SetPosition(0, firePoint.position);
+            lr.SetPosition(1, hitPos);
+        }
+
+        Destroy(trail.gameObject, 0.04f);
+
+        if (hitNormal != new Vector3(9999, 9999, 9999))
+        {
+            Transform hitParticle = Instantiate(HitPrefab, hitPos, Quaternion.FromToRotation(Vector3.right, hitNormal)) as Transform;
+            Destroy(hitParticle.gameObject, 1f);
+        }
+
+        Transform clone = Instantiate(MuzzleFlashPrefab, firePoint.position, firePoint.rotation) as Transform;
         clone.parent = firePoint;
-        float size = 0.01f * new Random().Next(60, 90);
+        float size = Random.Range(0.6f, 0.9f);
         clone.localScale = new Vector3(size, size, size);
         Destroy(clone.gameObject, 0.02f);
+
     }
 }
