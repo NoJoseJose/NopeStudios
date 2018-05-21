6ea53dbf7c570585507fdf23367eb3bb6e160367
Dylan Goldman <Dylan.Goldman4@gmail.com>
2018-05-20 22:21:31 -0500

    throwable?

---
+++ GameNumberOne/Assets/Throwable.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.UIElements;
+
+public class Throwable : MonoBehaviour
+{
+    private bool _hasBeenThrown;
+	void Start () 
+	{
+		
+	}
+	
+	void FixedUpdate () 
+	{
+	    if (!_hasBeenThrown && Input.GetKeyDown(KeyCode.Mouse0))
+	    {
+	        Rigidbody rb = GetComponent<Rigidbody>();
+            Debug.Log($"Throwing {rb.name}");
+	        _hasBeenThrown = true;
+	        rb.isKinematic = true;
+	        rb.useGravity = true;
+            rb.velocity = 100*Vector3.forward;
+	    }
+	}
+}
