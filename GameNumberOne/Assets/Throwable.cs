74d9a40f249b193d5a770dc378ed34cd7d2b031d
Dylan Goldman <Dylan.Goldman4@gmail.com>
2018-05-22 23:20:07 -0500

    fixing fixing fixing

--- GameNumberOne/Assets/Throwable.cs
+++ GameNumberOne/Assets/Throwable.cs
@@ -6,27 +6,33 @@ using UnityEngine.Experimental.UIElements;
 public class Throwable : MonoBehaviour
 {
     private bool _hasBeenThrown;
+    public Transform HandPosition;
 
-    void Start () 
+    void Awake () 
 	{
-		
-	}
+	    _hasBeenThrown = false;
+	    Rigidbody rb = GetComponent<Rigidbody>();
+	    rb.isKinematic = true;
+	    rb.useGravity = false;
+	    rb.velocity = new Vector3(0, 0, 0);
+    }
 	
 	void Update () 
 	{
 	    if (!_hasBeenThrown && Input.GetKeyDown(KeyCode.Mouse0))
 	    {
-	        GameObject clone = Instantiate(this.transform, this.transform.position, this.transform.rotation).gameObject;
             Rigidbody rb = GetComponent<Rigidbody>();
 	        //Collider cd = GetComponent<Collider>();
             Debug.Log($"Throwing {rb.name}");
 	        _hasBeenThrown = true;
+	        transform.parent = null;
 	        rb.isKinematic = false;
 	        rb.useGravity = true;
             rb.velocity = 10*Vector3.forward;
-	        //cd.enabled = true;
-            Instantiate(clone);
-	        clone.transform.parent = GameObject.FindGameObjectWithTag("ThrowingHand").transform;
+            //cd.enabled = true;
+	        GameObject clone = Instantiate(this.transform, this.transform.position, this.transform.rotation).gameObject;
+            clone.transform.parent = GameObject.FindGameObjectWithTag("ThrowingHand").transform;
+	        Throwable tb = clone.GetComponent<Throwable>();
 	    }
 	}
 }
