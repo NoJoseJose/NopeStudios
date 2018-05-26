35ed3814f2700395656a27e6a2fac837e2d77fa3
Unknown <theyurch@gmail.com>
2018-05-25 23:05:24 -0400

    character fail

---
+++ CharacterStudy/Assets/NetworkMessageHandler.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.Networking.NetworkSystem;
+
+//using joeythelantern's online tut basis for a manual network message
+
+public abstract class NetworkMessageHandler : NetworkBehaviour
+{
+    public const short movement_msg = 255;
+
+    public class PlayerMovementMessage : MessageBase
+    {
+        public string objectTransformName;
+        public Vector3 objectPosition;
+        public Quaternion objectRotation;
+        public float time;
+    }
+}
