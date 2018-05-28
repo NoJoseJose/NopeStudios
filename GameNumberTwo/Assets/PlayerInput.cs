fd436a96e59cb68beb50a712260cd19e92837225
Unknown <theyurch@gmail.com>
2018-05-28 10:50:38 -0400

    walkthing simple

---
+++ GameNumberTwo/Assets/PlayerInput.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour {
+
+    public ReactionController player;
+    float x, z = 0.0f;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        /*
+        x = Input.GetAxis("Horizontal");
+        z = Input.GetAxis("Vertical");
+        */
+
+        x = Input.GetAxis("Horizontal");
+        z = Input.GetAxis("Vertical");
+
+        player.desiredVelocity.x = x;
+        player.desiredVelocity.z = z;
+        player.desiredVelocity = player.desiredVelocity * player.maxDesiredSpeed;
+   
+    }
+
+}
---
+++ GameNumberTwo/Assets/Restart.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Restart : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+    void OnGUI()
+    {
+        if (GUILayout.Button("Restart"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+        // Update is called once per frame
+    void Update () {
+		
+	}
+}
