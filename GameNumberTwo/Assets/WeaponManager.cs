28e4e03dcf5e93288211829000e133ad30ec40f4
Dylan Goldman <Dylan.Goldman4@gmail.com>
2018-05-28 22:33:37 -0500

    gameweapons shell

---
+++ GameNumberTwo/Assets/WeaponManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponManager : MonoBehaviour 
+{
+	void Start () 
+	{
+		
+	}
+	
+	void Update ()
+	{
+	    ManageWeapons();
+	}
+
+    private void ManageWeapons()
+    {
+        if (Input.GetButtonDown("1"))
+        {
+
+
+        }
+    }
+}
---
+++ GameNumberTwo/GameWeapon.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class GameWeapon
+{
+    public string Name { get; set; }
+    public float Damage { get; set; }
+    public float FireRate { get; set; }
+    public float ThrowForce { get; set; }
+    public float Scale { get; set; }
+    public Transform Model { get; set; }
+}
