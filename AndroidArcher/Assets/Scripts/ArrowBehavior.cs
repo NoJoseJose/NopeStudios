b359b93d2691b749bf0dce87010ad1a18bcf48ba
Unknown <theyurch@gmail.com>
2019-01-13 14:23:52 -0500

    Arrow weathervanin'
    2

---
+++ AndroidArcher/Assets/Scripts/ArrowBehavior.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowBehavior : MonoBehaviour
+{
+    Rigidbody arrowBody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        arrowBody = this.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void FixedUpdate()
+    {
+        
+        
+        arrowBody.AddTorque(Vector3.Cross(arrowBody.transform.forward, arrowBody.velocity), ForceMode.Force);
+    }
+}
