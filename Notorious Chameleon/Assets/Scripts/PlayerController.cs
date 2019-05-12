6e8ba1033c0b1e87c38801f58f73eb29ccc44ef6
Unknown <dgoldman@accruent.com>
2019-05-12 16:50:45 -0500

    small updates

---
+++ Notorious Chameleon/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public static float CurrentHealth { get; set; } = MaxHealth;
+    public static float MaxHealth = 10f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ReceiveDamage(float damageVal)
+    {
+        CurrentHealth -= damageVal;
+        if (CurrentHealth <= 0)
+        {
+            Death();
+        }
+    }
+
+    private void Death()
+    {
+        Debug.Log("you died");
+    }
+}
