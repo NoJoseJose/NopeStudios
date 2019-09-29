1b45a4e44f58fe982fef33f5c13a6044da8730c2
Unknown <theyurch@gmail.com>
2019-09-28 22:00:46 -0500

    pushing this garbage so I can look at other stuff

---
+++ Notorious Chameleon/Assets/BalancePoint.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BalancePoint : MonoBehaviour
+{
+    public GameObject characterParent;
+    public float desiredHeight = 1.0f;
+
+
+    public Vector3 desiredVelocity = Vector3.zero;
+    public float maxDesiredSpeed = 10.0f;
+    public float maxAcceleration = 1.0f;
+
+    //public Rigidbody mainBody;
+    public Vector3 resultTilt;
+    // public Vector3 desiredAcceleration;
+
+    public float Kp = 1.0f;
+    public float Ki = 0.0f;
+    public float Kd = 0.0f;
+    Vector3 lastPos;
+    Vector3 lastError = Vector3.zero;
+    Vector3 P, I, D, seekDifference = Vector3.zero;
+    public bool isPlayer = false;
+
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void FixedUpdate()
+    {
+        /*
+        int bodies = 0;
+        Vector3 com = Vector3.zero;
+        foreach (Rigidbody body in characterParent.GetComponentsInChildren<Rigidbody>(true))
+        {
+            com += body.position * body.mass;
+            bodies++;
+        }
+        com /= bodies;
+        */
+        Vector3 com = characterParent.GetComponentInChildren<Rigidbody>().position + characterParent.GetComponentInChildren<Rigidbody>().centerOfMass;
+        Vector3 velocity = (com - lastPos) / Time.fixedDeltaTime;
+        lastPos = com;
+        P = (/*desiredVelocity*/  - velocity);
+
+
+        I += P * Time.fixedDeltaTime;
+        D = (P - lastError) / Time.fixedDeltaTime;
+        lastError = P;
+        seekDifference = P * Kp + I * Ki + D * Kd;
+        if (seekDifference.magnitude > maxAcceleration)
+        {
+            seekDifference = seekDifference.normalized * maxAcceleration;
+        }
+        resultTilt = new Vector3(0, -9.8f, 0) + seekDifference;
+
+    }
+    void OnDrawGizmos()
+    {
+        
+        Gizmos.color = Color.blue;
+        Gizmos.DrawRay(lastPos, resultTilt);
+        Gizmos.color = Color.black;
+        Gizmos.DrawRay(lastPos, seekDifference);
+        
+    }
+}
--- Notorious Chameleon/Assets/TwoJointSolve.cs
+++ Notorious Chameleon/Assets/TwoJointSolve.cs
@@ -17,11 +17,15 @@ public class TwoJointSolve : MonoBehaviour
     public Vector3 secondGimbalAxis = Vector3.up;
     //public Vector3 
     public Transform target;
+ //   public Transform inverseTarget;
     float dist1, dist2 = 0.1f;
+
+ //   public bool inverseAnchor = false; //inverse mode should make the joint calc as if it's anchored on the other side - moving torso with the feet, for instance
+                                        //you should probably move the target if you're doing this
 
     //visualizer
     Vector3 vv1, vv2, vv3, vv4;
-    
+    Vector3 vv1u, vv2u, vv3u, vv4u;
     //structure assumptions:
     //firstJoint and secondJoint transforms are at or roughly at the joint rotation
     //the system gets really confused if everything isn't in-line (or capable of) with each other (start joint -> mid joint -> end effector)
@@ -30,12 +34,14 @@ public class TwoJointSolve : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //calculate the distance we have to work with - I'm assuming this doesn't change. 
+        //calculate the distance we have to work with - I'm assuming this doesn't change. (maybe I shouldn't)
         dist1 = Vector3.Distance(firstJoint.transform.position, secondJoint.transform.position);
         dist2 = Vector3.Distance(secondJoint.transform.position, endPoint.position);
-        
-        firstRotationalOffset = Quaternion.LookRotation(endPoint.position - firstJoint.transform.position, firstJoint.transform.TransformVector(firstJoint.secondaryAxis));
-        secondRotationalOffset = Quaternion.LookRotation(endPoint.position - secondJoint.transform.position, secondJoint.transform.TransformVector(secondJoint.secondaryAxis));
+
+        //firstRotationalOffset = Quaternion.LookRotation(endPoint.position - firstJoint.transform.position, firstJoint.transform.TransformVector(firstJoint.secondaryAxis));
+        //secondRotationalOffset = Quaternion.LookRotation(endPoint.position - secondJoint.transform.position, secondJoint.transform.TransformVector(secondJoint.secondaryAxis));
+        firstRotationalOffset = Quaternion.LookRotation(endPoint.position - firstJoint.transform.position, firstRotationalAxis * firstGimbalAxis);
+        secondRotationalOffset = Quaternion.LookRotation(endPoint.position - secondJoint.transform.position, secondRotationalAxis * secondGimbalAxis);
     }
 
     // Update is called once per frame
@@ -57,7 +63,18 @@ public class TwoJointSolve : MonoBehaviour
         float angle, angle2;
         angle = 0.0f;
         angle2 = 0.0f;
-        float targetDist = Vector3.Distance(firstJoint.transform.position, target.position);
+        float targetDist;
+    /*    if (inverseAnchor)
+        {
+            //inv anchor to target point
+            targetDist = Vector3.Distance(inverseTarget.position, target.position);
+        }
+        else
+        */
+        {
+            //start of joint to target point
+            targetDist = Vector3.Distance(firstJoint.transform.position, target.position);
+        }
 
         if (targetDist < dist1 + dist2)
         {
@@ -66,32 +83,70 @@ public class TwoJointSolve : MonoBehaviour
 
             angle = Mathf.Acos(((dist1 * dist1) + (targetDist * targetDist) - (dist2 * dist2)) / (2 * dist1 * targetDist)) * Mathf.Rad2Deg;
             //the triangle determines the foldback angle
-            //angle2 = 180 - Mathf.Acos(((dist2 * dist2) + (dist1 * dist1) - (targetDist * targetDist)) / (2 * dist2 * dist1)) * Mathf.Rad2Deg;
+            angle2 = 180 - Mathf.Acos(((dist2 * dist2) + (dist1 * dist1) - (targetDist * targetDist)) / (2 * dist2 * dist1)) * Mathf.Rad2Deg;
 
-
+            
         }
 
         //basis from the parent
         firstRotationalAxis = firstJoint.transform.parent.rotation;
         secondRotationalAxis = secondJoint.transform.parent.rotation;
-        
-        //point a rotation along the limb segment.
-        Quaternion firstRotation = Quaternion.LookRotation(target.position - firstJoint.transform.position, firstRotationalAxis * firstGimbalAxis);
-        Quaternion secondRotation = Quaternion.LookRotation(target.position - secondJoint.transform.position, secondRotationalAxis * secondGimbalAxis);
-        
+
+        Quaternion firstRotation;
+        Quaternion secondRotation;
+        /*
+        if (inverseAnchor)
+        {
+            //this is strange - get the correct angles for where we want to be
+            firstRotation = Quaternion.LookRotation(target.position - inverseTarget.position, firstRotationalAxis * firstGimbalAxis);
+            
+            secondRotation = firstRotation;
+            //secondRotation = Quaternion.LookRotation(target.position - secondJoint.transform.position, secondRotationalAxis * secondGimbalAxis);
+            //the second is more or less neutral?
+        }
+        else
+        */
+        {
+            //point a rotation along the limb segment.
+            firstRotation = Quaternion.LookRotation(target.position - firstJoint.transform.position, firstRotationalAxis * firstGimbalAxis);
+            secondRotation = Quaternion.LookRotation(target.position - firstJoint.transform.position, secondRotationalAxis * secondGimbalAxis);
+            //secondRotation = firstRotation;
+        }
+
+
+        /*
+        if (inverseAnchor)
+        {
+            secondRotation *= Quaternion.AngleAxis(angle2 - angle,  firstRotation * -elbowAxis);
+        }
+ 
+        else
+    */
+        Debug.Log(angle + ", " + angle2);
+
+
         //do a bend. Currently leave the second half to find it's own way.
-        firstRotation *= Quaternion.AngleAxis(angle, firstRotation * elbowAxis);
-        //secondRotation *= Quaternion.AngleAxis(angle2, secondRotation * -elbowAxis); 
-        
+        firstRotation *= Quaternion.AngleAxis(angle, firstJoint.transform.rotation * elbowAxis); //why this is flipping?
+        secondRotation *= Quaternion.AngleAxis(angle - angle2, firstJoint.transform.rotation * elbowAxis);
+
+
+  
         //apply the starting limb conditions
         firstRotation *= Quaternion.Inverse(firstRotationalOffset);
         secondRotation *= Quaternion.Inverse(secondRotationalOffset);
+
+        
 
         //apply the rotations needed to get where desired and the parent start
         firstJoint.targetRotation = Quaternion.Inverse(firstRotation) * firstRotationalAxis;
         secondJoint.targetRotation = Quaternion.Inverse(secondRotation) * secondRotationalAxis;
 
-        
+        vv1 = firstRotation * Vector3.forward * dist1;
+        vv1u = firstRotationalAxis * firstGimbalAxis;
+        vv2 = secondRotation * Vector3.forward * dist2;
+        vv2u = secondRotationalAxis * secondGimbalAxis;
+        vv3 = firstRotation * elbowAxis;
+
         //joint.targetRotation = Quaternion.Inverse(Quaternion.Euler(new Vector3(x, y, z)));
         //Debug.Log("1: " + angle + " 2: " + angle2);
 
@@ -101,16 +156,31 @@ public class TwoJointSolve : MonoBehaviour
     {
         Gizmos.color = Color.blue;
         //Gizmos.DrawSphere(secondJoint.transform.position, 0.3f);
-        if(vv1 != null)
+        if(vv1 != null && vv1u != null)
         {
+
+            
             Gizmos.color = Color.blue;
-            Gizmos.DrawRay(secondJoint.transform.position, vv1 * 1.5f);
+            Gizmos.DrawRay(firstJoint.transform.position, vv1);
+     //       Gizmos.DrawRay(inverseTarget.position, vv1);
+            Gizmos.color = Color.red;
+            Gizmos.DrawRay(firstJoint.transform.position, vv1u);
+     //       Gizmos.DrawRay(inverseTarget.position, vv1u);
+            
         }
-        if (vv2 != null)
+        if (vv2 != null && vv2u != null && vv3 != null)
         {
             
             Gizmos.color = Color.green;
-            Gizmos.DrawRay(secondJoint.transform.position, vv2 * 1.5f);
+            Gizmos.DrawRay(secondJoint.transform.position, vv2);
+     //       Gizmos.DrawRay(inverseTarget.position + vv1, vv2);
+            Gizmos.color = Color.red;
+            Gizmos.DrawRay(secondJoint.transform.position, vv2u);
+    //        Gizmos.DrawRay(inverseTarget.position + vv1, vv2u);
+
+            Gizmos.color = Color.magenta;
+            Gizmos.DrawRay(secondJoint.transform.position, vv3);
+    //        Gizmos.DrawRay(inverseTarget.position + vv1, vv3);
         }
     }
 }
